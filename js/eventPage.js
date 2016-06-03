2406c659f9616fe0170c60c58fe248a8beb1c70a
luwenxull <992273307@qq.com>
2016-06-03 17:12:48 +0800

    test of download without saveas but failed

--- js/eventPage.js
+++ js/eventPage.js
@@ -1,10 +1,12 @@
 chrome.runtime.onMessage.addListener(function(msg,sender){
 	console.log(msg);
 	console.log(sender);
+	var blob=new Blob(['https://pic3.zhimg.com/585d17746_l.jpg'],{type:'image/*'});
+	var url=URL.createObjectURL(blob);
 	chrome.downloads.download(
 		{
-			url:'https://pic3.zhimg.com/585d17746_l.jpg',
-			filename:'./tttt.jpg',
+			url:url,
+			// filename:'tttt',
 			saveAs:false
 		}, 
 		function(){
